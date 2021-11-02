9945ca6a2c64ac1f5235e7073c543c6ed8bd3177
Aishik Mukherjee <aishikm2002@gmail.com>
2021-11-02 18:54:54 +0530

    Add linkedList.c

---
+++ linkedList.c
@@ -0,0 +1,58 @@
+/*
+C program to store four elements in a linked list
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <math.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    //Create integer type variables that
+    int a, b, c, d;
+
+    //Create the nodes
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *last;
+
+    //Allocate heap memory for nodes
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    last = (struct Node *)malloc(sizeof(struct Node));
+
+    //Take user input for values of all the variables
+    printf("Enter the first element to store in the linked list:\n");
+    scanf("%d", &a);
+    printf("Enter the second element to store in the linked list:\n");
+    scanf("%d", &b);
+    printf("Enter the third element to store in the linked list:\n");
+    scanf("%d", &c);
+    printf("Enter the fourth element to store in the linked list:\n");
+    scanf("%d", &d);
+
+    //Link one node to the next
+    head->data = a;
+    head->next = second;
+
+    head->data = a;
+    head->next = third;
+
+    head->data = a;
+    head->next = last;
+
+    head->data = a;
+    head->next = NULL;
+
+    return 0;
+}
