65deff80405a48eaa46ee7e96743651225581f0b
Aishik Mukherjee <aishikm2002@gmail.com>
2021-11-02 20:07:14 +0530

    Create linkedListBeginning.c
    * Also updated readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Programming learning progress
-This repository stores all the files I created while learning programming from scratch
+
+* This repository stores all the files I created while learning programming from scratch
+* Please ignore my noob mistakes :)
---
+++ linkedListBeginning.c
@@ -0,0 +1,89 @@
+/*
+C program to insert a new node at the beginning of the linked list
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <math.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void traversePrint(struct Node *ptr)
+{
+    int i = 1;
+    while (ptr != NULL)
+    {
+        printf("The element number %d is: %d\n", i, ptr->data);
+        ptr = ptr->next;
+        i++;
+    }
+}
+
+struct Node *insertBeginning(struct Node *head, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    ptr->next = head;
+    return ptr;
+}
+
+int main()
+{
+    //Create integer type variables that
+    int a, b, c, d, z;
+
+    //Create the nodes
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *last;
+
+    //Allocate heap memory for nodes
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    last = (struct Node *)malloc(sizeof(struct Node));
+
+    //Take user input for values of all the variables
+    printf("Enter the first element to store in the linked list:\n");
+    scanf("%d", &a);
+    printf("Enter the second element to store in the linked list:\n");
+    scanf("%d", &b);
+    printf("Enter the third element to store in the linked list:\n");
+    scanf("%d", &c);
+    printf("Enter the fourth element to store in the linked list:\n");
+    scanf("%d", &d);
+
+    //Link one node to the next
+    head->data = a;
+    head->next = second;
+
+    second->data = b;
+    second->next = third;
+
+    third->data = c;
+    third->next = last;
+
+    last->data = d;
+    last->next = NULL;
+
+    //Traverse and print elements inside a node
+    traversePrint(head);
+
+    //Take user input for the new element
+    printf("Enter the new element to insert in the beginning:\n");
+    scanf("%d", &z);
+
+    //Declare the new head for the insertion of the new node in the beginning
+    head = insertBeginning(head, z);
+
+    traversePrint(head);
+
+    return 0;
+}
