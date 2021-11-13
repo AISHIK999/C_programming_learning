b82bc1b69b9a15dcbf2c40525b2590e5473ec26d
Aishik Mukherjee <aishikm2002@gmail.com>
2021-11-13 10:14:21 +0530

    Traverse circular liked list

---
+++ circularLinkedTraverse.c
@@ -0,0 +1,73 @@
+/*
+C prpgram to create and print out a circular linked list
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <math.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void traversePrint(struct Node * head){
+    struct Node * ptr = head;
+    int i = 0;
+    do
+    {
+        i++;
+        printf("The element number %d is: %d\n", i, ptr -> data);
+        ptr = ptr -> next;
+    }while(ptr != head);
+    
+}
+
+int main()
+{
+    //Create integer type variables that
+    int a, b, c, d;
+
+    //Create the nodes
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *last;
+
+    //Allocate heap memory for nodes
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    last = (struct Node *)malloc(sizeof(struct Node));
+
+    //Take user input for values of all the variables
+    printf("Enter the first element to store in the linked list:\n");
+    scanf("%d", &a);
+    printf("Enter the second element to store in the linked list:\n");
+    scanf("%d", &b);
+    printf("Enter the third element to store in the linked list:\n");
+    scanf("%d", &c);
+    printf("Enter the fourth element to store in the linked list:\n");
+    scanf("%d", &d);
+
+    //Link one node to the next
+    head->data = a;
+    head->next = second;
+
+    second->data = b;
+    second->next = third;
+
+    third->data = c;
+    third->next = last;
+
+    last->data = d;
+    last->next = NULL;
+
+    //Traverse and print elements inside a node
+    traversePrint(head);
+
+    return 0;
+}
